3de32d539d5d17f4cdec13fc5fb06f96daae9db7
JossPerezT <jossperez44@gmail.com>
2022-04-13 23:51:16 -0500

    update main.js

--- weekly_mission_1/example9/main.js
+++ weekly_mission_1/example9/main.js
@@ -1,1 +1,19 @@
-const Pokemon = require ('')
+const Pokemon = require ('./pokemon.js')
+
+const pikachu = new Pokemon ("pikachu")
+const bulbasaur = new Pokemon ("balbasur")
+const squirtle = new Pokemon ("squirtle")
+const charmander = new Pokemon ("charmander")
+
+
+pikachu.sayHello()
+pikachu.sayMessage ("Heeeey!")
+
+bulbasaur.sayHello()
+bulbasaur.sayMessage ("Heeeeeey!")
+
+charmander.sayHello()
+charmander.sayMessage("Heeeeeeeeeeey!")
+
+squirtle.sayHello()
+squirtle.sayMessage("Heeeeeeeeeeeeeeeeeeeeeey!")
