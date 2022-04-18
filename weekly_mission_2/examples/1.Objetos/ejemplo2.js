3c331069b4b0d951c2e4d7d819e2b3a7635403f0
JossPerezT <jossperez44@gmail.com>
2022-04-18 17:04:42 -0500

    Ejecucuión de los ejercicios de objetos

---
+++ weekly_mission_2/examples/1.Objetos/ejemplo2.js
@@ -0,0 +1,6 @@
+const myObject2 = {
+    name: "Joss" ,
+    age: 23
+}
+console.log ("Ejemplo 2: Crear un objeto con propiedades")
+console.log (myObject2)
---
+++ weekly_mission_2/examples/1.Objetos/ejemplo3.js
@@ -0,0 +1,23 @@
+// Ejemplo 3: Objeto con diferentes propiedades 
+const myObject3 = {
+    name: "Hachi",
+    age: 7 ,
+    nicknames: [
+        "Hachito",
+        "Hacho", 
+        "Hachiko",
+        "Hachitito"
+    ],
+    address: {
+        zip_code: "10212",
+        street: "Jose Miguel Cruz",
+        city: "Estado de México"
+    }
+}
+
+
+console.log ("Ejemplo 3: Objeto con diferentes propiedades")
+console.log (myObject3)
+console.log (myObject3.name)
+console.log (myObject3["address"])
+console.log (myObject3.nicknames)
