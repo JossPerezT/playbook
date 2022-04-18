afaad1a2ad2b0475bc365f828ba443103d73e8db
JossPerezT <jossperez44@gmail.com>
2022-04-17 21:43:16 -0500

    Agregando el archivo demo 1_window_global_values.js*

---
+++ weekly_mission_2/ejemplos_lives/1_window_global_values.js
@@ -0,0 +1,8 @@
+/*
+Contexto global: Al decarar las varibles en const o let, estaremos guardando los valores en un contexto global. Se pueden acceder a estos con el objeto window en el navegador
+*/
+
+a = `JavaScript`
+b = 10 
+
+console.log (a, b)
