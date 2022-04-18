ced03203d92a21f7873dff9b2e4c6e1854cb2445
JossPerezT <jossperez44@gmail.com>
2022-04-18 16:39:57 -0500

    Create files Objects

---
+++ weekly_mission_2/examples/1.Objetos/ejemplo1.js
@@ -0,0 +1,6 @@
+console.log ("Objetos")
+
+// Ejemplo 1: Crear un objeto 
+const myObject = {} //Así se declara un objeto vacio 
+console.log ("Ejemplo 1: Crear un objegto vacío")
+console.log(myObject)
